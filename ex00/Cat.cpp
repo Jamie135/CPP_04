8f3c8f2ab7beacec9792d6e34a38b74b2d15fd27
unknown <jamie.gworld@hotmail.com>
2023-09-20 21:45:15 +0700

    OK

---
+++ ex00/Cat.cpp
@@ -0,0 +1,28 @@
+#include "Cat.hpp"
+
+Cat::Cat() : Animal("Cat")
+{
+	std::cout << "Cat Constructor called" << std::endl;
+}
+
+Cat::Cat(Cat &copy) : Animal(copy.type)
+{
+	std::cout << "Cat Copy constructor called" << std::endl;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat " << type << " is destroyed" << std::endl;
+}
+
+Cat& Cat::operator=(const Cat& src)
+{
+	std::cout << "Cat: Copy assignment operator called" << std::endl;
+	type = src.type;
+	return (*this);
+}
+
+void	Cat::makeSound(void) const
+{
+	std::cout << "Cat: Moew !!" << std::endl;
+}
---
+++ ex00/Dog.cpp
@@ -0,0 +1,33 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal("Dog")
+{
+	std::cout << "Dog Constructor called" << std::endl;
+}
+
+Dog::Dog(const std::string &type) : Animal(type)
+{
+	std::cout << "Dog Constructor called" << std::endl;
+}
+
+Dog::Dog(Dog &copy) : Animal(copy.type)
+{
+	std::cout << "Dog Copy constructor called" << std::endl;
+}
+
+Dog::~Dog()
+{
+	std::cout << "Dog: " << type << " is destroyed" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& src)
+{
+	std::cout << "Dog: Copy assignment operator called" << std::endl;
+	type = src.type;
+	return (*this);
+}
+
+void	Dog::makeSound(void) const
+{
+	std::cout << "Dog: Bark !!" << std::endl;
+}
