514ad776fba91e29835bbbad088e2b04a078875b
unknown <jamie.gworld@hotmail.com>
2023-09-20 23:27:23 +0700

    OK

---
+++ ex01/Cat.cpp
@@ -0,0 +1,47 @@
+#include "Cat.hpp"
+#include <string>
+
+Cat::Cat() : Animal("Cat"), brain(new Brain())
+{
+	std::cout << "Cat: " <<  "Constructor called" << std::endl;
+}
+
+Cat::Cat(Cat &copy) : Animal(copy.type), brain(new Brain((*copy.getBrain())))
+{
+	std::cout << "Cat: Constructor called" << std::endl;
+}
+
+Cat::~Cat()
+{
+	delete brain;
+	std::cout << "Cat " << type << " is destroyed" << std::endl;
+}
+
+Cat& Cat::operator=(const Cat& src)
+{
+	std::cout << "Cat: Copy assignment operator called" << std::endl;
+	if (&src != this)
+	{
+		this->type = src.type;
+		if (brain)
+			delete brain;
+		brain = new Brain((*src.getBrain()));
+	}
+	return (*this);
+}
+
+void	Cat::makeSound(void) const
+{
+	std::cout << "Cat: Moew!" << std::endl;
+}
+
+Brain*	Cat::getBrain(void) const
+{
+	return (brain);
+}
+
+void	Cat::setIdeas(const std::string &idea)
+{
+	for (int i = 0; i < 100; i++)
+		brain->setIdea(i, idea);
+}
---
+++ ex01/Dog.cpp
@@ -0,0 +1,47 @@
+#include "Dog.hpp"
+#include <string>
+
+Dog::Dog() : Animal("Dog"), brain(new Brain())
+{
+	std::cout << "Dog: " << "Constructor called" << std::endl;
+}
+
+Dog::Dog(Dog &copy) : Animal("Dog"), brain(new Brain((*copy.getBrain())))
+{
+	std::cout << "Dog: Copy constructor called" << std::endl;
+}
+
+Dog::~Dog()
+{
+	delete brain;
+	std::cout << "Dog: " << type << " is destroyed" << std::endl;
+}
+
+Dog& Dog::operator=(const Dog& src)
+{
+	std::cout << "Dog: Copy assignment operator called" << std::endl;
+	if (&src != this)
+	{
+		type = src.type;
+		if (brain)
+			delete brain;
+		brain = new Brain((*src.getBrain()));
+	}
+	return (*this);
+}
+
+void	Dog::makeSound(void) const
+{
+	std::cout << "Dog: Woof!" << std::endl;
+}
+
+Brain*	Dog::getBrain(void) const
+{
+	return (brain);
+}
+
+void	Dog::setIdeas(const std::string &idea)
+{
+	for (int i = 0; i < 100; i++)
+		brain->setIdea(i, idea);
+}
